fb5503ab61e4db0f7f0d8b40b75a0eb02b427508
Kim Alex <kimalex2004dev@gmail.com>
2024-04-16 01:31:31 +0900

    Changed logic to work preset id dto

--- src/prompting/prompting.service.ts
+++ src/prompting/prompting.service.ts
@@ -5,6 +5,7 @@ import * as FormData from 'form-data';
 import * as fs from 'fs';
 import { v4 as uuid } from 'uuid';
 import { PresetHandler } from './preset/presetHandler';
+import { UserRequestPresetIdDto } from './dto/userRequestPresetIdDto';
 
 @Injectable()
 export class PromptingService {
@@ -13,23 +14,21 @@ export class PromptingService {
     this.axiosInstance = axios.create();
   }
 
+  // TODO: TEST
   async imageToImageV16(
-    userPrompt: string,
-    preset: string,
-    weight: number,
+    userRequestPresetId: UserRequestPresetIdDto,
     image: Express.Multer.File,
   ) {
-    const chosenPreset = PresetHandler.handlePreset(preset);
+    const chosenPreset = PresetHandler.handlePreset(userRequestPresetId);
 
-    const promptToSend = `${userPrompt} ${chosenPreset}`;
-    console.log(promptToSend);
+    console.log(chosenPreset);
     if (!image) {
       throw new HttpException('Image is not embedded', HttpStatus.BAD_REQUEST);
     }
 
     const formData = new FormData();
-    formData.append('text_prompts[0][text]', promptToSend);
-    formData.append('text_prompts[0][weight]', weight);
+    formData.append('text_prompts[0][text]', chosenPreset);
+    formData.append('text_prompts[0][weight]', 1);
     formData.append('init_image', image.buffer, {
       filename: image.originalname,
     });
@@ -83,74 +82,74 @@ export class PromptingService {
     }
   }
 
-  async imageToImageXL(
-    userPrompt: string,
-    preset: string,
-    weight: number,
-    image: Express.Multer.File,
-  ) {
-    const chosenPreset = PresetHandler.handlePreset(preset);
-
-    const promptToSend = `${userPrompt} ${chosenPreset}`;
-    console.log(promptToSend);
-    if (!image) {
-      throw new HttpException('Image is not embedded', HttpStatus.BAD_REQUEST);
-    }
-
-    const formData = new FormData();
-    formData.append('text_prompts[0][text]', promptToSend);
-    formData.append('text_prompts[0][weight]', weight);
-    formData.append('init_image', image.buffer, {
-      filename: image.originalname,
-    });
-    formData.append('init_image_mode', 'IMAGE_STRENGTH');
-    formData.append('image_strength', 0.09);
-    formData.append('cfg_scale', 19);
-    formData.append('clip_guidance_preset', 'SLOW');
-    formData.append('samples', 1);
-    formData.append('seed', 0);
-    formData.append('steps', 50);
-    formData.append('sampler', 'K_HEUN');
-    console.log('Sending request...');
-
-    try {
-      const response = await this.axiosInstance.postForm(
-        'https://api.stability.ai/v1/generation/stable-diffusion-xl-1024-v1-0/image-to-image',
-        formData,
-        {
-          headers: {
-            Authorization: 'Bearer ' + process.env.API_KEY, // CREATE YOUR .ENV FILE AND PUT THERE API_KEY (STABILITY API KEY)
-            Accept: 'image/png',
-            ...formData.getHeaders(),
-          },
-          responseType: 'arraybuffer',
-        },
-      );
-
-      console.log('Response received');
-
-      const filename = 'XLresponse' + '-' + uuid() + '.png';
-      await this.saveImage(response.data, filename);
-
-      // transform arraybuffer to image
-
-      return Buffer.from(response.data, 'binary');
-    } catch (error) {
-      if (error.response) {
-        console.error('Error response:', error.response.data.toString());
-        console.error('Status code:', error.response.status);
-      } else if (error.request) {
-        console.error('No response received:', error.request);
-      } else {
-        console.error('Request error:', error.message);
-      }
-
-      throw new HttpException(
-        'Failed to generate image',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
+  // async imageToImageXL(
+  //   userPrompt: string,
+  //   preset: string,
+  //   weight: number,
+  //   image: Express.Multer.File,
+  // ) {
+  //   const chosenPreset = PresetHandler.handlePreset(preset);
+  //
+  //   const promptToSend = `${userPrompt} ${chosenPreset}`;
+  //   console.log(promptToSend);
+  //   if (!image) {
+  //     throw new HttpException('Image is not embedded', HttpStatus.BAD_REQUEST);
+  //   }
+  //
+  //   const formData = new FormData();
+  //   formData.append('text_prompts[0][text]', promptToSend);
+  //   formData.append('text_prompts[0][weight]', weight);
+  //   formData.append('init_image', image.buffer, {
+  //     filename: image.originalname,
+  //   });
+  //   formData.append('init_image_mode', 'IMAGE_STRENGTH');
+  //   formData.append('image_strength', 0.09);
+  //   formData.append('cfg_scale', 19);
+  //   formData.append('clip_guidance_preset', 'SLOW');
+  //   formData.append('samples', 1);
+  //   formData.append('seed', 0);
+  //   formData.append('steps', 50);
+  //   formData.append('sampler', 'K_HEUN');
+  //   console.log('Sending request...');
+  //
+  //   try {
+  //     const response = await this.axiosInstance.postForm(
+  //       'https://api.stability.ai/v1/generation/stable-diffusion-xl-1024-v1-0/image-to-image',
+  //       formData,
+  //       {
+  //         headers: {
+  //           Authorization: 'Bearer ' + process.env.API_KEY, // CREATE YOUR .ENV FILE AND PUT THERE API_KEY (STABILITY API KEY)
+  //           Accept: 'image/png',
+  //           ...formData.getHeaders(),
+  //         },
+  //         responseType: 'arraybuffer',
+  //       },
+  //     );
+  //
+  //     console.log('Response received');
+  //
+  //     const filename = 'XLresponse' + '-' + uuid() + '.png';
+  //     await this.saveImage(response.data, filename);
+  //
+  //     // transform arraybuffer to image
+  //
+  //     return Buffer.from(response.data, 'binary');
+  //   } catch (error) {
+  //     if (error.response) {
+  //       console.error('Error response:', error.response.data.toString());
+  //       console.error('Status code:', error.response.status);
+  //     } else if (error.request) {
+  //       console.error('No response received:', error.request);
+  //     } else {
+  //       console.error('Request error:', error.message);
+  //     }
+  //
+  //     throw new HttpException(
+  //       'Failed to generate image',
+  //       HttpStatus.INTERNAL_SERVER_ERROR,
+  //     );
+  //   }
+  // }
 
   private async saveImage(imageData: ArrayBuffer, filename: string) {
     try {
