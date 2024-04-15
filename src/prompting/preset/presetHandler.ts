326d41c87e47601b8c2bd8fcf94773b1e79f52ca
Kim Alex <kimalex2004dev@gmail.com>
2024-04-16 01:31:45 +0900

    Created preset handling logic

--- src/prompting/preset/presetHandler.ts
+++ src/prompting/preset/presetHandler.ts
@@ -1,3 +1,35 @@
+import { UserRequestPresetIdDto } from '../dto/userRequestPresetIdDto';
+import { Preset } from './presets';
 export class PresetHandler {
-  static handlePreset(preset: string): string {}
+  static handlePreset(presetId: UserRequestPresetIdDto): string {
+    let presetPrompt = '';
+
+    switch (presetId.genderId) {
+      case 'male':
+        presetPrompt += Preset.MALE;
+        break;
+      case 'female':
+        presetPrompt += Preset.FEMALE;
+    }
+
+    switch (presetId.poseId) {
+      case 'standing_straight':
+        presetPrompt += Preset.STANDING_STRAIGHT;
+        break;
+    }
+
+    switch (presetId.heightId) {
+      case 'short':
+        presetPrompt += Preset.SHORT;
+        break;
+      case 'mid':
+        presetPrompt += Preset.MID;
+        break;
+      case 'high':
+        presetPrompt += Preset.HIGH;
+        break;
+    }
+
+    return presetPrompt;
+  }
 }
