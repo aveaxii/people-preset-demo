1b61fb61a2c2e045d117572cb70680d8f65922da
Kim Alex <kimalex2004dev@gmail.com>
2024-04-15 20:16:39 +0900

    Added utility in module providers

--- src/prompting/prompting.module.ts
+++ src/prompting/prompting.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PromptingController } from './prompting.controller';
 import { PromptingService } from './prompting.service';
+import { Utility } from './util';
 
 @Module({
   controllers: [PromptingController],
-  providers: [PromptingService],
+  providers: [PromptingService, Utility],
 })
 export class PromptingModule {}
