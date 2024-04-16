28f1f8cc7c85198fe791fcf97a1c945ab4f02579
Kim Alex <kimalex2004dev@gmail.com>
2024-04-16 20:41:26 +0900

    Added new options

--- src/prompting/preset/presetHandler.ts
+++ src/prompting/preset/presetHandler.ts
@@ -16,6 +16,12 @@ export class PresetHandler {
       case 'standing_straight':
         presetPrompt += Preset.STANDING_STRAIGHT;
         break;
+      case 'standing_arms_together':
+        presetPrompt += Preset.STANDING_ARMS_TOGETHER;
+        break;
+      case 'standing_arms_on_sides':
+        presetPrompt += Preset.STANDING_ARMS_ON_SIDES;
+        break;
     }
 
     switch (presetId.heightId) {
