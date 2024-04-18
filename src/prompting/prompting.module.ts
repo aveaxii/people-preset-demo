a811e34346a2f3007b8b18168c5eda3d19098e87
Kim Alex <kimalex2004dev@gmail.com>
2024-04-18 15:19:41 +0900

    Added poses handler as module provider

--- src/prompting/prompting.module.ts
+++ src/prompting/prompting.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PromptingController } from './prompting.controller';
 import { PromptingService } from './prompting.service';
 import { Utility } from './util/util';
+import { PosesHandler } from './poses/posesHandler';
 
 @Module({
   controllers: [PromptingController],
-  providers: [PromptingService, Utility],
+  providers: [PromptingService, Utility, PosesHandler],
 })
 export class PromptingModule {}
