0e46cc88c56412eeb87e8653411dd65fbd1f337b
Kim Alex <kimalex2004dev@gmail.com>
2024-04-18 15:19:25 +0900

    Created poses enum and poses handler

---
+++ src/prompting/poses/poses.ts
@@ -0,0 +1,8 @@
+export enum Poses {
+  STANDING_STRAIGHT = 'standing_straight.png',
+  STANDING_ARMS_CROSSED = 'standing_arms_crossed.png',
+  STANDING_ARMS_ON_SIDES = 'standing_arms_on_sides.png',
+  STANDING_SLIGHTLY_SIDE = 'standing_slightly_side.png',
+  T_POSE = 't_pose.png',
+  FACE_CLOSE = 'face_close.png',
+}
---
+++ src/prompting/poses/posesHandler.ts
@@ -0,0 +1,46 @@
+import { Poses } from './poses';
+import * as path from 'path';
+import * as fs from 'fs';
+
+export class PosesHandler {
+  private readonly rootPath: string;
+  constructor(rootPath: string) {
+    this.rootPath = rootPath;
+  }
+  // async getPose(pose: string) {
+  //   const poseImagePath = this.getPoseImage(pose);
+  //   if (poseImagePath) {
+  //     try {
+  //       const imageData = fs.readFileSync(poseImagePath);
+  //       return imageData;
+  //     } catch (error) {
+  //       throw new Error('Pose is not found');
+  //     }
+  //   }
+  // }
+  getPoseImage(posePath: string) {
+    switch (posePath) {
+      case 'standing_straight':
+        return this.getPoseImagePath(Poses.STANDING_STRAIGHT);
+      case 'standing_arms_crossed':
+        return this.getPoseImagePath(Poses.STANDING_ARMS_CROSSED);
+      case 'standing_arms_on_sides':
+        return this.getPoseImagePath(Poses.STANDING_ARMS_ON_SIDES);
+      case 'standing_slightly_side':
+        return this.getPoseImagePath(Poses.STANDING_SLIGHTLY_SIDE);
+      case 't_pose':
+        return this.getPoseImagePath(Poses.T_POSE);
+      case 'face_close':
+        return this.getPoseImagePath(Poses.FACE_CLOSE);
+      default:
+        throw new Error('Pose image not found');
+    }
+  }
+  private getPoseImagePath(pose: Poses): string {
+    const skeletonsFolderPath = 'C:\\TestProjects\\people-preset\\skeletons'; // Абсолютный путь к папке skeletons
+    console.log('skeletonsFolderPath: ' + skeletonsFolderPath);
+    const imagePath = path.join(skeletonsFolderPath, pose);
+    console.log('imagePath: ' + imagePath);
+    return imagePath;
+  }
+}
